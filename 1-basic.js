9b5a40ab316e33c7a35081b08343088c86b8877e
spenstock <44307894+spenstock@users.noreply.github.com>
2020-07-12 21:55:28 -0500

    added tictactoe to projects

--- 1-basic.js
+++ 1-basic.js
@@ -1,43 +1,57 @@
 const menu = [
   {
     id: 1,
+    title: "tictactoe",
+    website: "https://github.com/spenstock/tictactoe",
+    time: 2020,
+    desc: `Google Chrome Web Extention, Popup in Browser, Javascript`,
+  },
+  {
+    id: 2,
     title: "map",
+    website: "mapfolder/map.html",
     time: 2020,
     desc: `Map distance project, utilizing Leaflet, OpenStreetMap API`,
   },
   {
-    id: 2,
+    id: 3,
     title: "submarine",
+    website: "submarinefolder/submarine.html",
     time: 2019,
     desc: `Deliver the seaweed to the ship to win! HTML5 Canvas`,
   },
   {
-    id: 3,
+    id: 4,
     title: "jungle",
+    website: "junglefolder/jungle.html",
     time: 2019,
     desc: `Climb on vines to Tarzan swing to the otherside! HTML5 Canvas`,
   },
   {
-    id: 4,
+    id: 5,
     title: "card",
+    website: "cardfolder/card.html",
     time: 2019,
     desc: `Matching card game! HTML5 Canvas`,
   },
   {
-    id: 5,
+    id: 6,
     title: "haunted",
+    website: "hauntedfolder/haunted.html",
     time: 2019,
     desc: `Make it though the Haunted Castle! HTML5 Canvas`,
   },
   {
-    id: 6,
+    id: 7,
     title: "snow",
+    website: "snowfolder/snow.html",
     time: 2019,
     desc: `Make it though the snow storm! HTML5 Canvas`,
   },
   {
-    id: 7,
+    id: 8,
     title: "desert",
+    website: "desertfolder/desert.html",
     time: 2019,
     desc: `Make it though the desert! (basic) HTML5 Canvas`,
   },
@@ -70,8 +84,9 @@ const menu = [
   //   desc: ``,
   // },
   {
-    id: 8,
+    id: 9,
     title: "lava",
+    website: "lavafolder/lava.html",
     time: 2019,
     desc: `Make it though the lava jump game! HTML5 Canvas`,
   },
@@ -85,7 +100,7 @@ window.addEventListener("DOMContentLoaded", function () {
     // console.log(item);
 
     return `<article class="menu-item">
-          <a href="${item.title}folder/${item.title}.html" target="_blank">
+          <a href="${item.website}" target="_blank">
           <img src="./images/${item.title}.JPG" alt=${item.title} class="photo" />
           </a>
           <div class="item-info">
